a0c511a61b1cee1a0bbc72f67b224a7d384319d8
BhumikaGarg-02 <gargbhumi806@gmail.com>
2026-07-10 22:23:37 +0530

    Time: 19 ms (81.33%), Space: 70.4 MB (78.67%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private:
+    bool possible(vector<int>& bloomDay, int m, int k,int day){
+        int cnt=0;
+        int nob=0;
+        for(int i=0;i<bloomDay.size();i++){
+            if(bloomDay[i]<=day){cnt++;}
+            else{
+                nob += cnt/k;
+                cnt=0;
+            }
+        }
+        nob+=cnt/k;
+        if(nob>=m)return true;
+        else return false;
+    }
+    vector<int> mxmn(vector<int>& bloomDay){
+        int maxi=bloomDay[0];
+        int mini=bloomDay[0];
+        for(int i=1;i<bloomDay.size();i++){
+            if(bloomDay[i]>maxi){maxi=bloomDay[i];}
+            else if(bloomDay[i]<mini){mini=bloomDay[i];}
+        }
+        return {mini,maxi};
+    }
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        vector<int> v=mxmn(bloomDay);
+        int l=v[0],h=v[1];
+        int ans=-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(possible(bloomDay,m,k,mid)==true){
+                ans=mid;
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
