c8cfd0fed98c49477dcaffe27600fe5bbfc6a4f5
BhumikaGarg-02 <gargbhumi806@gmail.com>
2026-07-10 22:32:34 +0530

    Time: 17 ms (87.75%), Space: 70.5 MB (47.47%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -11,24 +11,19 @@ private:
             }
         }
         nob+=cnt/k;
-        if(nob>=m)return true;
-        else return false;
+        return nob>=m;
     }
-    vector<int> mxmn(vector<int>& bloomDay){
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+         if(bloomDay.size()<(long long)m*k){return -1;}
         int maxi=bloomDay[0];
         int mini=bloomDay[0];
         for(int i=1;i<bloomDay.size();i++){
             if(bloomDay[i]>maxi){maxi=bloomDay[i];}
             else if(bloomDay[i]<mini){mini=bloomDay[i];}
         }
-        return {mini,maxi};
-    }
-public:
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        vector<int> v=mxmn(bloomDay);
-        int l=v[0],h=v[1];
+        int l=mini,h=maxi;
         int ans=-1;
-        if(bloomDay.size()<(long long)m*k){return -1;}
         while(l<=h){
             int mid=(l+h)/2;
             if(possible(bloomDay,m,k,mid)==true){
