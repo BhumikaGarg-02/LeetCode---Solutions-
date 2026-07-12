612578545e05d7fdc610cb59b9c369ad97d9a9f3
BhumikaGarg-02 <gargbhumi806@gmail.com>
2026-07-12 22:33:27 +0530

    Time: 0 ms (100%), Space: 10.6 MB (99.98%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -6,18 +6,43 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+ #pragma GCC optimize("Ofast,unroll-loops,fast-math")
+#pragma GCC target("avx,avx2,fma,sse4,popcnt,lzcnt,bmi,bmi2")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+static constexpr size_t mxal = alignof(max_align_t);
+alignas(mxal) static unsigned char buf[256 * 1024 * 1024];
+static size_t pos = 0;
+
+void* operator new(size_t sz) {
+    size_t pad = (mxal - (pos % mxal)) % mxal;
+    pos += pad + sz;
+    return (void*)(&buf[pos - sz]);
+}
+
+void* operator new[](size_t sz) { return operator new(sz); }
+
+void operator delete(void*) noexcept {}
+void operator delete[](void*) noexcept {}
+void operator delete(void*, size_t) noexcept {}
+void operator delete[](void*, size_t) noexcept {}
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==NULL || head->next==NULL){return false;}
+        if(head==NULL){return false;}
         ListNode * fast=head;
         ListNode * slow=head;
         while(fast!=NULL && fast->next!=NULL){
                 fast=fast->next->next;
                 slow=slow->next;
-            if(fast==slow){
-                return true;
-            }
+            if(fast==slow)return true;
         }
         return false;
     }
