994b6b88049bcf861d7d434ba847696fb04be89a
BhumikaGarg-02 <gargbhumi806@gmail.com>
2026-07-13 15:08:38 +0530

    Time: 7 ms (66.83%), Space: 10.3 MB (99.98%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,14 +9,11 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(head==NULL ||head->next==NULL){return NULL;}
         ListNode * fast=head;
         ListNode * slow=head;
-        int cnt=0;
         while(fast!=NULL && fast->next!=NULL){
                 fast=fast->next->next;
                 slow=slow->next;
-                cnt++;
             if(fast==slow){
                 fast=head;
                 while(fast!=slow){
@@ -29,3 +26,4 @@ public:
         return NULL;
     }
 };
+const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
