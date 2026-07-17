6d20db88d46a1afe8c1fe2a7fc0146103dfdfc71
BhumikaGarg-02 <gargbhumi806@gmail.com>
2026-07-17 22:48:25 +0530

    Time: 0 ms (100%), Space: 14.2 MB (99.96%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,6 +8,33 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ #pragma GCC optimize("Ofast")
+
+#include <iostream>
+#include <vector>
+
+static constexpr std::size_t max_align = alignof(std::max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static std::size_t pos = 0;
+
+void *operator new(const std::size_t size) {
+    const std::size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return &BUFFER[pos - size];
+}
+
+void *operator new[](const std::size_t size) {
+    return operator new(size);
+}
+
+void operator delete(void *) noexcept {}
+
+void operator delete[](void *) noexcept {}
+
+void operator delete(void *, std::size_t) noexcept {}
+
+void operator delete[](void *, std::size_t) noexcept {}
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
