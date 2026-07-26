4a60bfd8ef172430ac8dcfda6c85035217bd7bd7
BhumikaGarg-02 <gargbhumi806@gmail.com>
2026-07-26 12:04:53 +0530

    Time: 0 ms (100%), Space: 8.7 MB (45.17%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -3,7 +3,7 @@ public:
     int titleToNumber(string columnTitle) {
         int sum=0;
         for(int i=0;i<columnTitle.size();i++){
-            sum=(sum*26)+(columnTitle[i]-'A'+1) ;
+            sum=(sum*26)+(columnTitle[i]-64) ;
         }
         return sum;
     }
