ee55c06652bc342823662b18e5450ecb0de6c780
BhumikaGarg-02 <gargbhumi806@gmail.com>
2026-08-01 21:09:03 +0530

    Time: 0 ms (100%), Space: 7.9 MB (12.6%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==0)return false;
+        else if(n==INT_MIN)return false;
+        else if((n&(n-1))==0)return true;
+        else return false;
+    }
+};
