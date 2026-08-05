bcf23cf4d7f9d9dbdc74285dd76803f82e58b612
BhumikaGarg-02 <gargbhumi806@gmail.com>
2026-08-05 17:04:02 +0530

    Time: 1 ms (71.53%), Space: 8.9 MB (75.63%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if(n<=0)return false;
-        while(n%3==0){n=n/3;}
-        return n==1;
+        while(n>1){
+            if(n%3!=0){
+                return false;
+            }
+            n=n/3;}
+        return true;
     }
 };
