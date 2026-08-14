5653c5468deaf55339549b5196d5ecc1078df761
BhumikaGarg-02 <gargbhumi806@gmail.com>
2026-08-14 17:06:38 +0530

    Time: 1 ms (57.72%), Space: 9 MB (95.5%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -4,20 +4,15 @@ public:
         vector<int>freq(26,0);
         int n=s.size();
         int maxi=INT_MIN;
-        int i=0;
         int start=0;
-        while(i<n){
-            while(i<n && freq[s[i]-'a']+1 <=2){
-                freq[s[i]-'a']++;
-                i++;
-            }
-            maxi=max(maxi,i-start);
-            while(i<n && start<i && freq[s[i]-'a']+1>2){
+        for(int i=0;i<n;i++){
+            freq[s[i]-'a']++;
+            while(freq[s[i]-'a']>2){
                 freq[s[start]-'a']--;
                 start++;
             }
+            maxi=max(maxi,i-start+1);
         }
-        maxi=max(maxi,i-start);
         return maxi;
     }
 };
