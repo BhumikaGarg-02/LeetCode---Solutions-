6313a731901fe70fa9d6de1c9ce784971d3584f0
BhumikaGarg-02 <gargbhumi806@gmail.com>
2026-08-31 20:32:33 +0530

    Time: 3 ms (67.35%), Space: 124.6 MB (60.57%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -15,23 +15,27 @@ public:
         ListNode* temp=head->next;
         ListNode* prev=head;
         ListNode* next=head->next->next;
-        vector<int>ans;
         int i=2;
+        int diff=INT_MAX;
+        int el=-1;
+        int mini=-1;
         while(next!=nullptr){
-            if(temp->val>next->val && temp->val>prev->val)ans.push_back(i);
-            else if(temp->val<next->val && temp->val<prev->val)ans.push_back(i);
+            if(temp->val>next->val && temp->val>prev->val){
+                if(el==-1)mini=i;
+                if(el!=-1)diff=min(diff,i-el);
+                el=i;
+                }
+            else if(temp->val<next->val && temp->val<prev->val){
+                if(el==-1)mini=i;
+                if(el!=-1)diff=min(diff,i-el);
+                el=i;
+                }
             i++;
             prev=temp;
             temp=next;
             next=next->next;
         }
-        if(ans.size()==0 || ans.size()==1)return{-1,-1};
-        int diff=INT_MAX;
-        int n=ans.size();
-        for(int i=0;i<n-1;i++){
-            diff=min(ans[i+1]-ans[i],diff);
-        }
-        int maxi=ans[n-1]-ans[0];
-        return{diff,maxi};
+        if(el==-1 || mini==el)return{-1,-1};
+        return{diff,el-mini};
     }
 };
